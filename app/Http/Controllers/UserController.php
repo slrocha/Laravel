18e757734c95b564a545ee5b92d8e81d4a4bd584
slrocha <slrocha17@gmail.com>
2016-08-29 16:55:18 -0300

    modificação na função getExport para arquivos do excel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,19 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use DB;
+use App\User;
+use Excel;
 
 class UserController extends Controller
 {
     public function index()
     {
         $users=DB::table('users')->get();
-        return view('vista', compact('users'));
-
-        Excel::create('lista de usarios Excel', function($excel) {
-	   		$excel->sheet('Excel sheet', function($sheet) {
-	        	$users= App\User::all();
-	        	$sheet->loadView('vista',['users'=> $users]);
-	    	});
-		})->export('csv');        
+        return view('export.exportToExcel', compact('users'));              
     } 
+
+    public function getExport(){
+        $user= User::all();
+        User::create('Usuários Inativos', function($excel)use($user) {
+            $excel->sheet('Excel sheet', function($sheet)use($user) {
+                //$users= App\User::all();
+                $sheet->fromArray($user);
+            });
+        })->export('xlsx');  
+
+
+
+    }
 }
